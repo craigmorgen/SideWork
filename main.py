a43c864bfa6cd52a121e55e66b3d2d0d4fd9fac1
Craig <35881089+thesupertoy@users.noreply.github.com>
2018-01-30 08:47:46 -0600

    Updated body variable data and requests.post.
    body = ({'message': desc}) #notice the addition parentheses
    requests.post updated to use json=body

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ desc = "You did it!"
 
 headers = {'Content-Type': 'application/json'}
 url = "http://127.0.0.1:8080/message/new"
-body = {'message': desc}
+body = ({'message': desc})
 
 r = requests.post(
     headers=headers,
     url=url,
-    data=body
+    json=body
 )
 
 print(body)
