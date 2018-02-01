10a960062ce88e68a3e7ef37b8047c6544320d64
Craig <35881089+thesupertoy@users.noreply.github.com>
2018-01-31 21:42:19 -0600

    Removed quotes variable and data.
    Deleted example comments.
    Updated batquote variable data.

--- insertData.py
+++ insertData.py
@@ -3,17 +3,9 @@ conn = sqlite3.connect('TheBats.db')
 
 c = conn.cursor()
 
-quotes = (
-        'Perhaps the lady is just what the doctor ordered?',
-        'The Bat-Signal is not a Beeper.',
-        'Look Bruce, Im a part of this whether you like it not.'
-)
+batquote = 'The Bat-Signal is not a Beeper.'
 
-batquote = ('I own a batcave, what have you got?')
-
-
-c.execute('INSERT INTO batquote (message) VALUES(?)', (batquote,))
-#cursor.execute('INSERT INTO images            VALUES(?)', (img,))
+c.execute('INSERT INTO batquote (message) VALUES (?)', (batquote,))
 
 conn.commit()
 
