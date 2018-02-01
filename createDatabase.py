0e257548e4784cb7604bc0f26f92c896860eafba
Craig <35881089+thesupertoy@users.noreply.github.com>
2018-01-31 21:40:27 -0600

    Updated INSERT INTO message data.
    Moved print statement to eliminate errors.

--- createDatabase.py
+++ createDatabase.py
@@ -9,14 +9,13 @@ c = conn.cursor()
 c.execute('CREATE TABLE batquote (id INTEGER PRIMARY KEY, message text)')
 
 # Insert a row of data
-c.execute("INSERT INTO batquote (message) VALUES ('i batman')")
+c.execute("INSERT INTO batquote (message) VALUES ('In the beginning, there was only Batman')")
 
 # Save (commit) the changes
 conn.commit()
 
+print(pd.read_sql_query('SELECT * FROM batquote', conn))
+
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
 conn.close()
-
-
-print(pd.read_sql_query('SELECT * FROM batquote', conn))
